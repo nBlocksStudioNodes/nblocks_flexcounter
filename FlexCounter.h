023c3618a6532cd8c3c537b946d7b3adea244aa7
nikos chalikias <nikos.chalikias@gmail.com>
2021-03-17 16:24:20 +0000

    node.json

--- FlexCounter.h
+++ FlexCounter.h
@@ -6,10 +6,9 @@
 class nBlock_FlexCounter: public nBlockSimpleNode<0> {
 
 public:
-    flexCounter(uint32_t maxvalue, uint32_t minvalue, uint32_t step, uint32_t mode);
+    nBlock_FlexCounter(uint32_t maxvalue, uint32_t minvalue, uint32_t step, uint32_t mode);
     void triggerInput(nBlocks_Message message);
 	void endFrame();
-};
 	   
 private:
 	uint32_t _count;
